811aaa3299cbc1055351c758e805ed7a04da04ad
zhangl <zhangl@dayukeji.xin>
2019-10-04 12:42:50 +0800

    提交交集，并集，差集笔记

--- 5.集合/index.js
+++ 5.集合/index.js
@@ -2,7 +2,7 @@
  * @Author: zhangl
  * @Date: 2019-10-03 17:12:42
  * @LastEditors: zhangl
- * @LastEditTime: 2019-10-03 17:43:31
+ * @LastEditTime: 2019-10-04 12:41:08
  * @Description: 集合
  */
 
@@ -53,6 +53,50 @@ var Set2 = function () {
             }
         }
         return values;
+    };
+
+    // union：并集
+    this.union = function (otherSet) {
+        var result = new Set2();
+        var setArr = otherSet.values();
+        var thisArr = this.values();
+
+        for (var i = 0; i < setArr.length; i++) {
+            result.add(setArr[i]);
+        }
+        for (var j = 0; j < thisArr.length; j++) {
+            result.add(thisArr[j]);
+        }
+
+        return result;
+    };
+
+    // intersection：交集
+    this.intersection = function (otherSet) {
+        var result = new Set2();
+        var thisArr = this.values();
+
+        for (var i = 0; i < thisArr.length; i++) {
+            if (otherSet.has(thisArr[i])) {
+                result.add(thisArr[i]);
+            }
+        }
+
+        return result;
+    };
+
+    // difference：差集
+    this.difference = function (otherSet) {
+        var result = new Set2();
+        var thisArr = this.values();
+
+        for (var i = 0; i < thisArr.length; i++) {
+            if (!otherSet.has(thisArr[i])) {
+                result.add(thisArr[i]);
+            }
+        }
+
+        return result;
     };
 
     // getItems
