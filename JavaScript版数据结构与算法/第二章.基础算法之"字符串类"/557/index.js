3aec89a4c9957c233c9b4332a988e6e848dbe39b
zhangl <zhangl@dayukeji.xin>
2019-10-04 16:03:59 +0800

    更新leetcode反转字符串中的单词 III代码

--- JavaScript版数据结构与算法/第二章.基础算法之"字符串类"/557/index.js
+++ JavaScript版数据结构与算法/第二章.基础算法之"字符串类"/557/index.js
@@ -2,7 +2,7 @@
  * @Author: zhangl
  * @Date: 2019-10-04 15:54:35
  * @LastEditors: zhangl
- * @LastEditTime: 2019-10-04 15:59:02
+ * @LastEditTime: 2019-10-04 16:03:31
  * @Description: 反转字符串中的单词 III
  */
 
@@ -13,9 +13,9 @@
 
 /**
  * 解题思路：
- * 1. 将字符串拆分成数组A
- * 2. 遍历数组A，对每一项，再进行拆分为数组B，使用reverse反转数组B，再将数组B转换为字符串
- * 3. 将数组A转换为字符串
+ * 1. 根据空格将字符串分割为数组A
+ * 2. 遍历数组A，对每一项，根据字符分割为数组B，使用reverse反转数组B，再将数组B还原为字符串
+ * 3. 最后数组A还原为字符串
  */
 
 const reverseWords = str => {
