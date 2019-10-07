47f4baaeba7a3e69601c956dc16e1e991c9736a4
zhangl <zhangl@dayukeji.xin>
2019-10-08 01:03:25 +0800

    提交第一个JS程序笔记

---
+++ Javascript完美收官/yj/3.第一个JS程序/code/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhangl
+ * @Date: 2019-10-08 00:33:24
+ * @LastEditors: zhangl
+ * @LastEditTime: 2019-10-08 00:57:05
+ * @Description: 第一个JS
+ */
+document.write('你好');
+alert('弹框显示');
+prompt('请输入你的年龄');
