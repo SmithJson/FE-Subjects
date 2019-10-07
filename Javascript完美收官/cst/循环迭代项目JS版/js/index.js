6be14b719916e154896b58183d64086890dc35e4
zhangl <zhangl@dayukeji.xin>
2019-10-08 00:15:38 +0800

    提交JS项目代码

--- Javascript完美收官/cst/循环迭代项目JS版/js/index.js
+++ Javascript完美收官/cst/循环迭代项目JS版/js/index.js
@@ -2,16 +2,15 @@
  * @Author: zhangl
  * @Date: 2019-10-06 20:23:04
  * @LastEditors: zhangl
- * @LastEditTime: 2019-10-06 23:20:15
+ * @LastEditTime: 2019-10-08 00:06:09
  * @Description: 项目脚本
  */
 var menu = document.getElementsByClassName('menu')[0];
 var modal = document.getElementsByClassName('modal')[0];
-var allData = [];
+var allTableData = [];
 
 function init() {
     var active = document.getElementsByClassName('active')[0];
-
     initContentShow(active);
     renderTable();
     bindEvent();
@@ -21,24 +20,12 @@ function init() {
 function bindEvent() {
     menu.onclick = function (e) {
         var tagName = e.target.tagName;
-
         if (tagName !== 'DD') return false;
         initContentShow(e.target);
         initMenu();
         e.target.classList.add('active');
     };
-
-    var editBtn = document.getElementsByClassName('edit');
     var modalContent = document.getElementsByClassName('modal-content')[0];
-
-    for (var i = 0; i < editBtn.length; i++) {
-        editBtn[i].onclick = function () {
-            var index = this.getAttribute('data-index');
-
-            modal.classList.add('show');
-            initEditForm(allData[index]);
-        };
-    }
     modalContent.onclick = function (e) {
         e.stopPropagation();
     };
@@ -47,14 +34,17 @@ function bindEvent() {
     };
     var editSubmitBtn = document.getElementById('edit-submit');
     var editForm = document.getElementById('edit-student-form');
-
     editSubmitBtn.onclick = function (e) {
         e.preventDefault();
+        var formResult = getFormData(editForm);
+        if (formResult.status === 'fail') {
+            alert(formResult.msg);
+            return false;
+        }
         var obj = Object.assign({}, {
             appkey: "dongmeiqi_1547441744650",
-        }, getFormData(editForm));
+        }, getFormData(editForm).student);
         var result = changeData("http://api.duyiedu.com/api/student/updateStudent", obj);
-
         if (result.status === 'success') { // 提交成功
             editForm.reset();
             alert(result.msg);
@@ -62,12 +52,33 @@ function bindEvent() {
             renderTable();
         }
     };
+    // 新增学生提交
+    var addSubmitBtn = document.getElementById('add-submit');
+    var addForm = document.getElementById('add-student-form');
+    addSubmitBtn.onclick = function (e) {
+        e.preventDefault();
+        var formResult = getFormData(addForm);
+        if (formResult.status === 'fail') {
+            alert(formResult.msg);
+            return false;
+        }
+        var studentData = formResult.student;
+        var obj = Object.assign({}, {
+            appkey: 'dongmeiqi_1547441744650',
+        }, studentData);
+        var result = changeData('http://api.duyiedu.com/api/student/addStudent', obj);
+        if (result.status === 'success') {
+            alert(result.msg);
+            window.location.reload();
+        } else {
+            alert(result.msg);
+        }
+    };
 }
 
 // 初始化菜单
 function initMenu() {
     var active = document.getElementsByClassName('active');
-
     for (var i = 0; i < active.length; i++) {
         active[i].classList.remove('active');
     }
@@ -78,7 +89,6 @@ function initContentShow(dom) {
     var id = dom.getAttribute('data-id');
     var rightContent = document.getElementsByClassName(id)[0];
     var content = document.getElementsByClassName('content');
-
     for (var i = 0; i < content.length; i++) {
         content[i].style.display = 'none';
     }
@@ -96,7 +106,7 @@ function changeData(url, param) {
     }
     if (typeof param == 'string') {
         xhr.open('GET', url + '?' + param, false);
-    } else if (typeof param == 'object'){
+    } else if (typeof param == 'object') {
         var str = "";
         for (var prop in param) {
             str += prop + '=' + param[prop] + '&';
@@ -119,42 +129,39 @@ function changeData(url, param) {
 // 渲染表格数据
 function renderTable() {
     var data = changeData("http://api.duyiedu.com/api/student/findAll", "appkey=dongmeiqi_1547441744650");
-
     if (data.status === 'fail') {
         alert(data.msg);
         return false;
     }
     var tableData = data.data;
-    allData = data.data;
+    allTableData = data.data;
     var tableBody = document.getElementById('student-body');
     var str = '';
-
     for (var i = 0; i < tableData.length; i++) {
         var item = tableData[i];
-
         str += '\
-            <tr>\
-                <td>'+ item.sNo +'</td>\
-                <td>' + item.name + '</td>\
-                <td>' + (item.sex ? '女' : '男') + '</td>\
-                <td>' + item.email + '</td>\
-                <td>' + (new Date().getFullYear() - item.birth) + '</td>\
-                <td>' + item.phone + '</td>\
-                <td>' + item.address + '</td>\
-                <td>\
-                    <button class="btn edit" data-index="'+ i +'">编辑</button>\
-                    <button class="btn del" data-index="'+ i +'">删除</button>\
-                </td>\
-            </tr>\
-        ';
+        <tr>\
+            <td>' + item.sNo + '</td>\
+            <td>' + item.name + '</td>\
+            <td>' + (item.sex ? '女' : '男') + '</td>\
+            <td>' + item.email + '</td>\
+            <td>' + (new Date().getFullYear() - item.birth) + '</td>\
+            <td>' + item.phone + '</td>\
+            <td>' + item.address + '</td>\
+            <td>\
+                <button class="btn edit" data-index="' + i + '">编辑</button>\
+                <button class="btn del" data-index="' + i + '">删除</button>\
+            </td>\
+        </tr>\
+    ';
     }
     tableBody.innerHTML = str;
+    bindTableEvent();
 }
 
 // 初始化编辑表单
 function initEditForm(data) {
     var editForm = document.getElementById('edit-student-form');
-
     for (var prop in data) {
         if (editForm[prop]) {
             editForm[prop].value = data[prop];
@@ -164,6 +171,8 @@ function initEditForm(data) {
 
 // 获取form表单的学生数据
 function getFormData(formData) {
+    var status = 'success';
+    var msg = '';
     var name = formData.name.value;
     var sex = formData.sex.value;
     var sNo = formData.sNo.value;
@@ -180,7 +189,40 @@ function getFormData(formData) {
         phone: phone,
         address: address,
     }
-    return student;
+    if (!name.match(/^\w{4,16}$/)) {
+        msg = '姓名为4 - 16位字符'
+        status = 'fail';
+    }
+    return {
+        student: student,
+        msg: msg,
+        status: status,
+    };
+}
+
+function bindTableEvent() {
+    var editBtn = document.getElementsByClassName('edit');
+    var delBtn = document.getElementsByClassName('del');
+    for (var i = 0; i < editBtn.length; i++) {
+        editBtn[i].onclick = function () {
+            var index = this.getAttribute('data-index');
+            modal.classList.add('show');
+            initEditForm(allTableData[index]);
+        };
+        delBtn[i].onclick = function () {
+            var isDel = window.confirm('确认删除？');
+            var index = this.getAttribute('data-index');
+            if (!isDel) return false;
+            var result = changeData('http://api.duyiedu.com/api/student/delBySno', {
+                appkey: 'dongmeiqi_1547441744650',
+                sNo: allTableData[index].sNo,
+            });
+            if (result.status === 'success') {
+                alert(result.msg);
+                renderTable();
+            }
+        };
+    }
 }
 
 init();
