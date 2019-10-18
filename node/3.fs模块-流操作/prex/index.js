e9eec8e4130950f2eb5ee0c1e7ee3aea23b3475b
zhangl <zhangl@dayukeji.xin>
2019-10-18 23:45:48 +0800

    提交文件监听

--- node/3.fs模块-流操作/prex/index.js
+++ node/3.fs模块-流操作/prex/index.js
@@ -2,21 +2,23 @@
  * @Author: zhangl
  * @Date: 2019-10-18 23:24:49
  * @LastEditors: zhangl
- * @LastEditTime: 2019-10-18 23:32:56
+ * @LastEditTime: 2019-10-18 23:44:11
  * @Description: 介绍
  */
 const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 const fs = require('fs')
 
-fs.readFile('./index.css', 'utf-8', (err, css) => {
-    postcss([autoprefixer])
-        .process(css, {
-            from: './index.css',
-            to: './newTest.css'
-        })
-        .then(result => {
-            console.log(result.css)
-            fs.writeFile('./newTest.css', result.css, () => true)
-        })
-})
+fs.watch('./index.css', 'utf-8', function (eventName, filename) {
+    console.log(eventName, filename);
+    fs.readFile('./' + filename, 'utf-8', (err, css) => {
+        postcss([autoprefixer])
+            .process(css, {
+                from: './' + filename,
+                to: './newTest.css'
+            })
+            .then(result => {
+                fs.writeFile('./newTest.css', result.css, () => true)
+            })
+    })
+});
