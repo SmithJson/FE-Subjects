97ed4e544fd26c927b357a959ab21d46c4a38af6
zhangl <zhangl@dayukeji.xin>
2019-11-10 02:05:56 +0800

    提交文件上传样式代码

---
+++ JS高级进阶/13.访问器/code/文件上传/js/index.js
@@ -0,0 +1,5 @@
+(function (global) {
+    const MAX_SIZE = 4 * 1024 * 1024; // 4MB 文件最大值
+    const MIN_SIZE = 2 * 1024; // 2KB 文件最小值
+    const fileData = []; // 上传的文件数据
+})(window);
---
+++ JS高级进阶/13.访问器/index.md
@@ -0,0 +1,22 @@
+# 访问器属性
+
+[toc]
+
+
+## get
+
+访问属性
+
+## set
+
+设置属性
+
+## enumerable
+
+可枚举
+
+## configurable
+
+可配置
+
+
