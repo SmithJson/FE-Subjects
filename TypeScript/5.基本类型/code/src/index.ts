a2838aaa5d1752781edfe5480a0b9f4a69513e46
zhangl <zhangl@dayukeji.xin>
2019-11-17 01:23:03 +0800

    提交 ts 基本类型笔记

---
+++ TypeScript/5.基本类型/code/src/index.ts
@@ -0,0 +1,17 @@
+// 约束写法一
+// 数字型数组
+let nums: number[];
+
+// nums = [1, 2, ,true]; error
+nums = [1, 2, 3]; // right
+
+// 约束写法二
+let nums2: Array<number> = [3, 4, 5];
+
+let u: object;
+u = {
+    name: 'zhangl',
+    age: 18,
+};
+
+let str: null = null;
---
+++ TypeScript/5.基本类型/index.md
@@ -0,0 +1,13 @@
+# 基本类型 {ignore=true}
+
+- number：数字
+- string：字符串
+- boolean：布尔
+- 数组
+- object：对象
+- null 和 undefined
+
+null 和 undefined 是所有类型的子类型，它可以赋值给其他类型
+
+通过添加 ```"strictNullChecks": true```，可以获得更严格的空类型检查
+null 和 undefined 只能赋值给自身
