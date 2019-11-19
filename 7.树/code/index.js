43178b0e6ad78dcffab03f4f72b0c75ffc0aedbb
zhangl <zhangl@dayukeji.xin>
2019-11-20 00:07:15 +0800

    提交树节点的遍历函数

--- 7.树/code/index.js
+++ 7.树/code/index.js
@@ -6,6 +6,7 @@ var Tree = function () {
         this.right = null;
     };
     var root = null;
+    // 插入节点辅助函数
     var insertNode = function (root, newNode) {
         if (root.value > newNode.value) {
             if (root.left === null) {
@@ -21,7 +22,16 @@ var Tree = function () {
             }
         }
     };
+    // 遍历树节点
+    var traverse = function (node, callback) {
+        if (node === null) return false;
+        // 后序遍历 左右根
+        traverse(node.left, callback);
+        traverse(node.right, callback);
+        callback(node.value);
+    };
 
+    // 插入元素
     this.insert = function (value) {
         var newNode = new Node(value);
 
@@ -30,6 +40,16 @@ var Tree = function () {
         } else {
             insertNode(root, newNode);
         }
+    };
+
+    // 查询元素
+    this.search = function (value) {
+
+    };
+
+    // 遍历元素
+    this.traverse = function (callback) {
+        traverse(root, callback);
     };
 
     this.getRoot = function () {
