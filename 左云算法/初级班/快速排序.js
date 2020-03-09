a9723d38ba8a96fa85af7814aaf51c4519b6e962
zhangl <zhangl@dayukeji.xin>
2020-03-10 00:32:12 +0800

    修改快速排序为随机快速排序

--- 左云算法/初级班/快速排序.js
+++ 左云算法/初级班/快速排序.js
@@ -6,37 +6,38 @@ function swap(arr, i, j) {
 	arr[j] = temp;
 }
 
-function partition(arr, L, R) {
-	var less = L - 1;
-	var more = R;
-	var current = L;
-	while (current < more) {
-		if (arr[current] < arr[R]) {
-			swap(arr, ++less, current++);
-		} else if (arr[current] > arr[R]) {
-			swap(arr, --more, current);
+function partition(arr, l, r) {
+	var less = l - 1;
+	var more = r;
+	while (l < more) {
+		if (arr[l] < arr[r]) {
+			swap(arr, ++less, l++);
+		} else if (arr[l] > arr[r]) {
+			swap(arr, --more, l);
 		} else {
-			current++;
+			l++;
 		}
 	}
-	swap(arr, more, R);
-	return [less, more];
+	swap(arr, more, r);
+	return [less + 1, more]; // 返回等于区域的左右边界
 }
 
-function quickSort(arr, L, R) {
-	if (L >= R) {
+function quicksort(arr, l, r) {
+	if (l >= r) {
 		return;
 	}
-	var p = partition(arr, L, R);
-	quickSort(arr, L, p[0]);
-	quickSort(arr, p[1] + 1, R);
+    // 随机快排
+    swap(arr, l + Math.floor(Math.random() * (r - l + 1)), r);
+	var p = partition(arr, l, r);
+	quicksort(arr, l, p[0] - 1);
+	quicksort(arr, p[1] + 1, r);
 }
 
 function sort(arr) {
 	if (arr === 0 || arr.length < 2) {
 		return arr;
 	}
-	quickSort(arr, 0, arr.length - 1);
+	quicksort(arr, 0, arr.length - 1);
 }
 
 sort(arr);
