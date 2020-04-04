55397527658210003281297cbf1ab5e1098db726
zhangl <zhangl@mail.ncut.edu.cn>
2020-04-05 01:07:13 +0800

    准备 redux 练习环境

---
+++ react-lesson/5.redux-demo/react-counter/src/App.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: zhangl
+ * @Date: 2020-04-05 00:58:23
+ * @LastEditors: zhangl
+ * @LastEditTime: 2020-04-05 01:02:07
+ * @GitHub: https://github.com/SmithJson
+ * @FilePath: /react-counter/src/App.js
+ * @Description: Do not edit
+ */
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ react-lesson/5.redux-demo/react-counter/src/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: zhangl
+ * @Date: 2020-04-05 00:58:23
+ * @LastEditors: zhangl
+ * @LastEditTime: 2020-04-05 01:05:20
+ * @GitHub: https://github.com/SmithJson
+ * @FilePath: /react-counter/src/index.js
+ * @Description: Do not edit
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
